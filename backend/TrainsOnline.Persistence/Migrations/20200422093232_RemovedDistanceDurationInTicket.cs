f643a9debf533a99180a16d57e186740ebac5bc5
Adam Bajguz <adam.bajguz@gmail.com>
2020-05-23 18:33:44 +0200

    Serilog packages

--- backend/TrainsOnline.Persistence/Migrations/20200422093232_RemovedDistanceDurationInTicket.cs
+++ backend/TrainsOnline.Persistence/Migrations/20200422093232_RemovedDistanceDurationInTicket.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace TrainsOnline.Persistence.Migrations
+﻿namespace TrainsOnline.Persistence.Migrations
 {
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class RemovedDistanceDurationInTicket : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
